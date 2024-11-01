723d262d4bd0c8dde139b5e0827ce0531c29c527
Dmitry Pustovit <DmitryPustovit@users.noreply.github.com>
2024-11-01 00:39:56 -0700

    Added Write Buffer

--- src/PinDefinitionsAndMore.h
+++ src/PinDefinitionsAndMore.h
@@ -15,15 +15,19 @@
 #define ping 0
 #define set_ignore_ir_reflection 1
 #define set_ir_mode 2
-#define enable_ir_buffer 3
+#define enable_ir_receiver_buffer 3
 #define set_ir_address 4
 #define get_ir_address 5
 #define clear_ir_receive_buffer 6
 #define get_ir_receive_buffer_avaliable 7
 #define read_ir_byte 8
 #define write_ir_byte 9
-#define set_ir_write_cache 10
-#define get_ir_write_cache 11
+#define write_to_ir_write_buffer 10
+#define get_ir_write_buffer 11
+#define get_ir_write_buffer_avaliable 12
+#define clear_ir_write_buffer 13
+#define set_ir_write_buffer_address 14
+#define get_ir_write_buffer_address 15
 
 // Mode Definitions
 #define ir_public_address 0
