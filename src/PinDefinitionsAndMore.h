01c4c60de28be0804df5285629b117617a4d248c
Dmitry Pustovit <me@dmitrypustovit.com>
2024-11-01 22:53:15 -0700

    Update PinDefinitionsAndMore.h

--- src/PinDefinitionsAndMore.h
+++ src/PinDefinitionsAndMore.h
@@ -14,13 +14,13 @@
 // I2C Command Definitions
 #define ping 0 // Validated - dsp
 #define set_ignore_ir_reflection 1
-#define set_ir_mode 2
+#define set_ir_mode 2 // Validated - dsp
 #define enable_ir_receiver_buffer 3 // Validated - dsp
 #define set_ir_address 4 // Validated - dsp
 #define get_ir_address 5 // Validated - dsp
 #define clear_ir_receive_buffer 6 // Validated - dsp
 #define get_ir_receive_buffer_avaliable 7 // Validated - dsp
-#define read_ir_byte 8 
+#define read_ir_byte 8 // Validated - dsp
 #define write_ir_byte 9 // Validated - dsp
 #define write_to_ir_write_buffer 10 // Validated - dsp
 #define get_ir_write_buffer 11 // Validated - dsp
